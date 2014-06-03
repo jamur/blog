e8dcc741cf49108e61ee12dc8a9c8a02c5f0b8d4
Rafael Jamur <jamur@ufpr.br>
2014-06-03 02:53:50 -0300

    ateh o 6 feito, falta debulhar o 7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,14 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
-	def new
-		@article = Article.new
-	end
-
 	def show
 		@article = Article.find(params[:id])
 	end	
+
+
+	def new
+		@article = Article.new
+	end
 
 	def create
 		@article = Article.new(article_params)
